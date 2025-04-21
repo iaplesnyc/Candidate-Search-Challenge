57100b4b379583695d6a652f40008fa009e72003
iaplesnyc <iaplesnyc@gmail.com>
2025-04-21 19:59:24 -0400

    Update index.tsx for React 18 and improve ErrorPage

--- src/pages/ErrorPage.tsx
+++ src/pages/ErrorPage.tsx
@@ -1,8 +1,15 @@
+import { Link } from 'react-router-dom';
+
 const ErrorPage = () => {
   return (
-    <section>
-      <h1>404: Page Not Found</h1>
-      <h1> ¯\_(ツ)_/¯</h1>
+    <section style={{ textAlign: 'center', padding: '3rem', color: 'white' }}>
+      <h1 style={{ fontSize: '3rem', marginBottom: '1rem' }}>404: Page Not Found</h1>
+      <p style={{ fontSize: '2rem' }}>¯\_(ツ)_/¯</p>
+      <p style={{ marginTop: '2rem' }}>
+        <Link to="/" style={{ color: 'lightblue', textDecoration: 'underline' }}>
+          Go back home
+        </Link>
+      </p>
     </section>
   );
 };
